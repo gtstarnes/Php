bd88ef51e26fc5c37f46512dfd09c8147ae0383d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-16 17:44:31 -0400

    created 404 page

---
+++ Lesson2/views/404.php
@@ -0,0 +1,11 @@
+
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+
+<main>
+    <h1>404 PAGE NOT FOUND</h1>
+    <p>
+        <a href="/">Go Back Home</a>
+    </p>
+</main>
+<?php require('partials/foot.php') ?>
