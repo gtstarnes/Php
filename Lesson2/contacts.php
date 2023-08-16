4a0fc25cca796721db339a674fc66369550d460f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-16 17:44:17 -0400

    removed duplicate files

--- Lesson2/contacts.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-require 'functions.php';
-
-$heading = 'Contact Us';
-
-require "views/contacts.view.php";
--- Lesson2/mission.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-require 'functions.php';
-
-$heading = 'Our Mission';
-
-require "views/mission.view.php";
