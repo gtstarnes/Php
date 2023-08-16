7cfddc942fd84259a92d1bccde0e9ca9c7bad7d4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-16 15:57:20 -0400

    created basic navigation

---
+++ Lesson2/about.view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about.php">About</a>
+        <a href="/mission.php">Mission</a>
+        <a href="/contacts.php">Contacts</a>
+    </nav>
+    <header>
+        <h1>About</h1>
+    </header>
+    <main>
+    <p>Now you are on the About Page.</p>
+    </main>
+</body>
+</html>
