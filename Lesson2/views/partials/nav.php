d9f8664fe08d8d2659645722b6ba033f28903b0b
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-16 16:57:12 -0400

    added conditional styling based on url

--- Lesson2/views/partials/nav.php
+++ Lesson2/views/partials/nav.php
@@ -1,6 +1,6 @@
 <nav>
-        <a href="/">Home</a>
-        <a href="/about.php">About</a>
-        <a href="/mission.php">Mission</a>
-        <a href="/contacts.php">Contacts</a>
+        <a href="/" style=<?= urlIs('/') ? "color:black" : ""  ?> >Home</a>
+        <a href="/about.php" style=<?= urlIs('/about.php') ? "color:black" : ""  ?>>About</a>
+        <a href="/mission.php" style=<?= urlIs('/mission.php') ? "color:black" : ""  ?>>Mission</a>
+        <a href="/contacts.php" style=<?= urlIs('/contacts.php') ? "color:black" : ""  ?>>Contacts</a>
 </nav>
