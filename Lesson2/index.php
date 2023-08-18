69e7c2a68c2083409e2a196e25f96eb6fe38d5a6
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-17 20:29:02 -0400

    setup access to mySQL database

--- Lesson2/index.php
+++ Lesson2/index.php
@@ -2,3 +2,16 @@
 
 require 'functions.php';
 require 'router.php'; 
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchALL(PDO::FETCH_ASSOC);
+
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['Title'] . "</li>";
+} 
