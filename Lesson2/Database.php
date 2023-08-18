f5b9a7b7337ed5615f830e9aae4431f8dc6a9d79
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-17 21:28:04 -0400

    refactored to make it more responsive

--- Lesson2/Database.php
+++ Lesson2/Database.php
@@ -2,10 +2,13 @@
 
 class Database { 
     public $connection;
-    public function __construct() {   
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+    public function __construct($config, $username = '', $password = '') {   
 
-        $this->connection = new PDO($dsn);  
+        $dsn = "mysql:" . http_build_query($config, '', ';');      
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);  
     }
     public function query($query){
         $statement = $this->connection->prepare($query);
--- Lesson2/index.php
+++ Lesson2/index.php
@@ -4,9 +4,10 @@ require 'functions.php';
 require 'router.php'; 
 require 'Database.php';
 
+$config = require('config.php');
 
-$db = new Database();
-$posts = $db->query("SELECT * FROM posts")->fetchALL(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
+$posts = $db->query("SELECT * FROM posts")->fetchALL();
 
 foreach ($posts as $post) {
     echo "<li>" . $post['Title'] . "</li>";
